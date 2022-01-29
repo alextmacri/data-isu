d04933b7b1af217ddf18097978fffcb004972a3a
Alex Macri <alexmacri04@gmail.com>
2022-01-28 23:50:48 -0500

    restarted django site

--- src/data_management/asgi.py
+++ src/data_management/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dataman project.
+ASGI config for data_management project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dataman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_management.settings')
 
 application = get_asgi_application()
--- src/data_management/wsgi.py
+++ src/data_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dataman project.
+WSGI config for data_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dataman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_management.settings')
 
 application = get_wsgi_application()
